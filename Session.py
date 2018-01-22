9f27187db239579ce39907a189417bff0f9af3b4
Edgar Solis <edgar21dejunio@gmail.com>
2018-01-22 17:56:03 -0600

    New Session module
    To allow major updates to the UI during execution
    a Session object must be the one holding the data and UI.
    This so it can be restarted while keeping the main script
    running.

---
+++ Session.py
@@ -0,0 +1,18 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+import Buttons
+
+
+class Session(QWidget):
+    """Holds all data and can be restarted to show changes."""
+
+    def __init__(self, parent):
+        """Init."""
+        super().__init(parent)
+
+        self.initUi()
+
+    def initUi(self):
+        """Ui generator."""
+        pass
