09f8698b4da2a961da23d3dca5b64bb518d320e0
Edgar Solis <edgar21dejunio@gmail.com>
2018-01-26 19:50:45 -0600

    New modules Reservation an Room
    Each module wil contain its data and graphic representation.
    
    Now they are just placeholders.

---
+++ Reservation.py
@@ -0,0 +1,19 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+
+class ReservationD(QWidget):
+    """Data representation of a reservation."""
+
+    def __init__(self, parent):
+        """Init."""
+        super().__init__(parent)
+
+
+class ReservationUi(QWidget):
+    """Graphic representation of a reservation."""
+
+    def __init__(self, parent):
+        """Init."""
+        super().__init__(parent)
---
+++ Room.py
@@ -0,0 +1,19 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+
+class RoomD(QWidget):
+    """Data representation of a room."""
+
+    def __init__(self, parent):
+        """Init."""
+        super().__init__(parent)
+
+
+class RoomUi(QWidget):
+    """Graphic representation of a room."""
+
+    def __init__(self, parent):
+        """Init."""
+        super().__init__(parent)
