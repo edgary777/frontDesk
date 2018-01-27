babb6873992b47144daac8d6b84b2c7601215e84
Edgar Solis <edgar21dejunio@gmail.com>
2018-01-26 19:49:32 -0600

    Initial skeleton setup of the classes

--- Dashboard.py
+++ Dashboard.py
@@ -14,7 +14,20 @@ class DashScroll(QWidget):
 
     def initUi(self):
         """Ui setup."""
-        pass
+        layout = QVBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setSpacing(0)
+        self.area = QScrollArea()
+
+        # This must be done so it can update dynamically
+        self.area.setWidgetResizable(True)
+
+        layout.addWidget(self.area)
+        self.setLayout(layout)
+
+    def setScrollItem(self, item):
+        """Set the item for the scroll area."""
+        self.area.setWidget(item)
 
 
 class DashList(QWidget):
@@ -24,30 +37,73 @@ class DashList(QWidget):
         """Init."""
         super().__init__(parent)
 
+        self.items = []
+
         self.initUi()
 
     def initUi(self):
         """Ui setup."""
+        pass
+
+    def addItem(self):
+        """Add an item."""
+        pass
+
+    def removeItem(self):
+        """Remove an item."""
+        pass
+
+    def addMultiple(self):
+        """Add multiple items."""
+        pass
+
+    def removeMultiple(self):
+        """Remove multiple items."""
+        pass
+
+    def modifyItem(self):
+        """Modify an item."""
+        pass
+
+    def cleanUi(self):
+        """Remove all items from the layout."""
+        pass
+
+    def fillUi(self):
+        """Add all items in the items list to the layout."""
+        pass
+
+    def updateUi(self):
+        """Update the Ui."""
         pass
 
 
 class DashItemUi(QWidget):
     """Dashboard item graphic representation."""
 
-    def __init__(self, parent):
+    def __init__(self, parent, complex=False):
         """Init."""
         super().__init__(parent)
 
         self.initUi()
 
-    def initUi(self):
-        """Ui setup."""
+    def IOUi(self):
+        """Check-In and Check-Out Ui setup."""
+        # No de cuarto, nombre, No de noches, fecha de entrada, fecha de salida
+        pass
+
+    def statusUi(self):
+        """Room status Ui setup."""
+        # No de cuarto, fecha de entrada, fecha de salida, status
         pass
 
 
-class DashItem(object):
+class DashItem(QWidget):
     """Dashboard item data representation."""
 
-    def __init__(self, parent):
+    def __init__(self, parent, room=None, reservation=None):
         """Init."""
         super().__init__(parent)
+
+        self.reservation = reservation
+        self.room = room
