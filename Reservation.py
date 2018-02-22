7b6e1621d469dc03f351e15320f6b4f16ff5f2e0
Edgar Solis <edgar21dejunio@gmail.com>
2018-02-22 16:16:42 -0600

    Updates to reflect changes done by Db and Manager.
    Updated modules:
     - Reservation
     - Room

--- Reservation.py
+++ Reservation.py
@@ -2,75 +2,140 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from DataItems import DataItem
+import Room
+import Db
+import datetime
+import Manager
 
 
 class ReservationD(DataItem):
     """Data representation of a reservation."""
 
-    def __init__(self, guest, rooms, user, parent):
+    def __init__(self, rsvID, cursor, parent):
         """Init."""
         super().__init__(parent)
-
-        self.guest = guest
-        self.rooms = rooms  # 1 reservation can comprise multiple rooms.
-        self.user = user  # The user who made the reservation.
-
+        self.parent = parent
+        self.cursor = cursor
+        self.ID = rsvID
         # guestID == Passport, INE, driving license, etc...
         # otherGuests == People other than the one who made the reservation.
         self.items = [
-            "ID", "adults", "minors", "guestID", "dateIn", "dateOut", "rate",
-            "paid", "status", "company", "extras", "group", "notes",
-            "otherGuests"
+            "rsvID", "guestID", "statusID", "userID", "companyID", "adults",
+            "minors", "dateIn", "dateOut", "rate", "paid", "rsvgroup",
+            "notes", "registerDate"
         ]
 
         for item in self.items:
             setattr(self, item, None)
 
-    def getData(self):
-        """Return the reservation data as a dict.
+        self.createFromID()
 
-        Keys:
-        ID, guest, rooms, user, data, adults, minors,
-        guestID, dateIn, dateOut, rate, paid, status, company,
-        extras, group, notes, otherGuests
-        """
-        data = {}
+    def createFromID(self):
+        """Init room from ID."""
+        db = Db.Db()
+        rsvData = db.getReservation(self.ID, self.cursor)
+        x = 0
         for item in self.items:
-            data[item] = getattr(self, item)
+            setattr(self, item, rsvData[x])
+            x += 1
 
-        data["guest"] = self.guest
-        data["rooms"] = self.rooms
-        data["user"] = self.user
-        return data
+        # self.rsvID = self.ID
+        self.rooms = db.getReservationRooms(self.rsvID, self.cursor)
+        self.extras = db.getReservationExtras(self.rsvID, self.cursor)
+        self.otherGuests = db.getReservationOGuests(self.rsvID, self.cursor)
+        self.parking = db.getReservationParking(self.rsvID, self.cursor)
+        self.roomIDs = db.getReservationRoomsIDs(self.rsvID, self.cursor)
+        self.cursor = None
 
-    def assignRoom(self):
-        """Assign a room to the reservation."""
+    def assignRooms(self, cursor=None):
+        """Assign the rooms to the reservation."""
         # if there are rooms already occupied try and assign the room to the roomGroup
         # with the most occupied rooms. Rooms are grouped because they can share AC or
         # water heaters, so grouping them makes sense economically.
         # if there are no rooms already occupied, randomly assign it.
-        pass
+
+        # When instantiating a reservation it is passed a cursor object, but it is deleted
+        # after a while because the connectionto the db needs to be closed.
+        if self.cursor is None and not cursor is None:
+            self.cursor = cursor
+        else:
+            print("Missing Cursor on reservation assignRooms")
+            return
+        if len(self.roomIDs) == 0:
+            db = Db.Db()
+            roomIDs = []
+            for roomType in self.rooms:
+                for room in range(roomType[3]):
+                    din = datetime.datetime.strptime(self.dateIn, "%Y-%m-%d")
+                    dout = datetime.datetime.strptime(self.dateOut, "%Y-%m-%d")
+                    roomIDs.append(db.getBestRoom(din, dout, roomType[2], cursor))
+            for room in roomIDs:
+                db.insertRsvRoomID(self.rsvID, room, self.cursor)
+            self.roomIDs = db.getReservationRoomsIDs(self.rsvID, self.cursor)
+        self.cursor = None
 
     def getGuestName(self):
         """Return formatted name."""
-        return self.guest.getName()
+        db = Db.Db()
+        startConnection = db.startConnection()
+        connection = startConnection[0]
+        cursor = startConnection[1]
+
+        guest = db.getGuest(self.guestID, cursor)
+        db.endConnection(connection)
+        return guest[2] + ", " + guest[1]
 
     def getNights(self):
         """Return the number of nights the guest is staying."""
-        pass
+        return 1
 
     def getTotal(self):
         """Return the total amount to be paid by the guest."""
-        return self.getNights * self.rate
+        return self.getNights() * self.rate
 
     def getOwed(self):
         """Return the amount owed by the guest."""
-        pass
+        return 1
 
     def updatePaid(self):
         """Update the amount paid by the guest."""
-        pass
+        return 1
 
+    def getMultiRoom(self):
+        """Return room number."""
+        if len(self.rooms) > 1:
+            return len(self.rooms)
+        else:
+            return False
+
+    def getRoomsIDs(self):
+        """Return rooms IDs."""
+        return self.roomIDs
+
+    def getExtras(self):
+        """Return the reservation extras"""
+        return self.extras
+
+    def gotNote(self):
+        """Return True if there is a note, False if there isn't one."""
+        if self.notes is not None:
+            if self.notes != "":
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def getRoomForDashboard(self, parent):
+        """Return room object for dashboard when not multiroom."""
+        db = Db.Db()
+        startConnection = db.startConnection()
+        connection = startConnection[0]
+        cursor = startConnection[1]
+
+        room = Room.RoomD(self.roomIDs[0][2], cursor, parent)
+        db.endConnection(connection)
+        return room
 
 class ReservationUi(QWidget):
     """Graphic representation of a reservation."""
--- Room.py
+++ Room.py
@@ -2,23 +2,57 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from DataItems import DataItem
+import Db
 
 
 class RoomD(DataItem):
     """Data representation of a room."""
 
-    def __init__(self, parent):
+    def __init__(self, roomNo, cursor, parent):
         """Init."""
         super().__init__(parent)
 
+        self.ID = roomNo
+
+        self.cursor = cursor
+
         self.items = [
-            "ID", "number", "type", "beds", "maxCapacity", "extras", "status",
-            "notes"
+            "roomNo", "roomType", "roomTypeDesc", "beds", "maxCapacity", "extras", "status",
+            "notes", "roomTypeLongDesc"
         ]
 
         for item in self.items:
             setattr(self, item, None)
 
+        self.roomFromId()
+
+    def roomFromId(self):
+        """Init room from ID."""
+        db = Db.Db()
+
+        roomData = db.getRoom(self.ID, self.cursor)
+        roomTypeData = db.getRoomType(roomData[1], self.cursor)
+        roomExtrasData = db.getRoomExtras(self.ID, self.cursor)
+
+        self.roomNo = self.ID
+        self.roomType = roomTypeData[0]
+        self.roomTypeDesc = roomTypeData[1]
+        self.beds = roomTypeData[2]
+        self.maxCapacity = roomTypeData[3]
+        self.roomTypeLongDesc = roomTypeData[4]
+        self.extras = [room[0] for room in roomExtrasData]
+        self.status = roomData[2]
+        self.notes = roomData[4]
+
+    def gotNote(self):
+        """Return True if there is a note, False if there isn't one."""
+        if self.notes is not None:
+            if self.notes != "":
+                return True
+            else:
+                return False
+        else:
+            return False
 
 
 class RoomUi(QWidget):
